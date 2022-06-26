4de23dacece53a84d3d3c05ebe14449f2a154ca6
renasfatah95 <83961057+renasfatah95@users.noreply.github.com>
2022-06-26 14:13:35 +0300

    Update payment.php

--- payment.php
+++ payment.php
@@ -20,7 +20,7 @@ class Payment
 
     public function __construct()
     {$this->client = new Client([
-            'base_uri' => 'https://uatgw1.nasswallet.com/payment/transaction/',
+            'base_uri' => 'https://gw-api.nasswallet.com/phase3/payment/transaction/login',
             'timeout' => '1000'
         ]); }
 
@@ -61,7 +61,7 @@ class Payment
 
         if ($response->responseCode == 0 && $response->data->transactionId) {
             
-            echo "https://uatcheckout1.nasswallet.com/payment-gateway?id={$response->data->transactionId}&token={$response->data->token}&userIdentifier={$this->username}";
+            echo "https://checkout.nasswallet.com/payment-gateway?id={$response->data->transactionId}&token={$response->data->token}&userIdentifier={$this->username}";
            
         }else {
             return "something went wrong!";
